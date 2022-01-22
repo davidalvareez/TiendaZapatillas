6e8f6e615a038c837962acbe2e9793af1d4ee0ee
David <6694.joan23@fje.edu>
2022-01-22 14:30:40 +0100

    carta dinamica

--- resources/views/mostrarZapatillas.blade.php
+++ resources/views/mostrarZapatillas.blade.php
@@ -5,7 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="../public/js/script.js"></script>
+    <link rel="stylesheet" href="{{asset('../public/css/style.css')}}">
     <title>Mostrar Zapatillas</title>
 </head>
 <body>
@@ -37,43 +39,48 @@
             @endforeach
         </table>
     </div>
-    <!-- DAVID AQUÍ HAZ LA CARTA SIN SER DINAMICO -->
+    <!-- CARTA DINAMICA -->
     <div class="cards">
+      @foreach($listaZapatillas as $zapatillas)
+          <div class="card">
+            <div class="card__image-holder">
+              <img class="card__image" src="https://source.unsplash.com/300x225/?wave" alt="wave" />
+            </div>
+            <div class="card-title">
+              <a href="#" class="toggle-info btn">
+                <span class="left"></span>
+                <span class="right"></span>
+              </a>
+              <h2>
+                {{$zapatillas->marca_zapatilla}} {{$zapatillas->modelo_zapatilla}}
+                <small>{{$zapatillas->color_zapatilla}}</small>
+                  <small>{{$zapatillas->precio_zapatilla}}€ </small>
 
-  <div class="card">
-    <div class="card__image-holder">
-      <img class="card__image" src="https://source.unsplash.com/300x225/?wave" alt="wave" />
-    </div>
-    <div class="card-title">
-      <a href="#" class="toggle-info btn">
-        <span class="left"></span>
-        <span class="right"></span>
-      </a>
-      <h2>
-          Nike Cortez
-          <small>Items sacados de la base de datos</small>
-      </h2>
-    </div>
-    <div class="card-flap flap1">
-      <div class="card-description">
-        Tallas disponibles (sacada de bbdd/sacada de bbdd/sacada de bbdd/)
-        <br>
-        <select>
-          <option>tallaBBDD</option>
-          <option>tallaBBDD</option>
-          <option>tallaBBDD</option>
-          <option>tallaBBDD</option>
-          <option>tallaBBDD</option>
-          <option>tallaBBDD</option>
-          <option>tallaBBDD</option>
-          <option>tallaBBDD</option>
-          <option>tallaBBDD</option>
-        </select>
-        <span class="left"></span>
-        <span class="right"></span>
-      </div>
-    </div>
-    </div>
-    </div>
-</body>
-</html>
+              </h2>
+            </div>
+            <div class="card-flap flap1">
+              <div class="card-description">
+                Tallas disponibles:
+                <select class="talla_zapatilla" name="talla_zapatilla">
+                  <option value="35">35</option>
+                  <option value="36">36</option>
+                  <option value="37">37</option>
+                  <option value="38">38</option>
+                  <option value="39">39</option>
+                  <option value="40">40</option>
+                  <option value="41">41</option>
+                  <option value="42">42</option>
+                  <option value="43">43</option>
+                  <option value="44">44</option>
+                  <option value="45">45</option>
+                </select>
+              </div>
+              <div class="card-flap flap2">
+                <div class="card-actions">
+                  <a href="#" class="btn">Añadir al carro</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        @endforeach
+      
