beaee78125d73b4eb5b0b8a4d7e804c0efbe2ba9
David <6694.joan23@fje.edu>
2022-01-23 13:05:30 +0100

    desaparecer cartas carrito compra

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,38 @@
 $(document).ready(function() {
+
+    /*MODAL BOX*/
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+        document.getElementById('cards').style.display = "none"; // hide
+        modal.style.display = "block";
+
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        document.getElementById('cards').style.display = "block"; // visible
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            document.getElementById('cards').style.display = "block"; // visible
+            modal.style.display = "none";
+        }
+    }
+
+
+    /*Cartas*/
     var zindex = 10;
 
     $("div.card").click(function(e) {
--- resources/views/mostrarZapatillas.blade.php
+++ resources/views/mostrarZapatillas.blade.php
@@ -11,76 +11,73 @@
     <title>Mostrar Zapatillas</title>
 </head>
 <body>
-    <div class="menu" style="float: right">
-        <button>Iniciar sesion</button>
-        <button>icono cesta (desplegable modal box)</button>
+    <div class="menu">
+      <button>Iniciar sesion</button>
+      <button id="myBtn">icono carrito compra</button>
     </div>
-    <div>
-        <table class="table table-striped">
-            <tr>
-                <th>MARCA</th>
-                <th>MODELO</th>
-                <th>COLOR</th>
-                <th>TALLA</th>
-                <th>PRECIO</th>
-                <th>FOTO</th>
-                <th>ACCIÓN</th>
-            </tr>
-            @foreach($listaZapatillas as $zapatillas)
-                <tr>
-                    <td>{{$zapatillas->marca_zapatilla}}</td>
-                    <td>{{$zapatillas->modelo_zapatilla}}</td>
-                    <td>{{$zapatillas->color_zapatilla}}</td>
-                    <td>{{$zapatillas->talla_zapatilla}}</td>
-                    <td>{{$zapatillas->precio_zapatilla}}</td>
-                    <td>{{$zapatillas->foto_zapatilla}}</td>
-                    <td><button>Añadir carrito</button></td>
-                </tr>
-            @endforeach
-        </table>
-    </div>
-    <!-- CARTA DINAMICA -->
-    <div class="cards">
+    <div id="myModal" class="modal">
+      <div class="modal-content">
+        <span class="close">&times;</span>
+        <p>Some text in the Modal..</p>
+      </div>
+    </div>  
+    <div class="cards" id="cards">
       @foreach($listaZapatillas as $zapatillas)
-          <div class="card">
-            <div class="card__image-holder">
-              <img class="card__image" src="https://source.unsplash.com/300x225/?wave" alt="wave" />
-            </div>
-            <div class="card-title">
-              <a href="#" class="toggle-info btn">
-                <span class="left"></span>
-                <span class="right"></span>
-              </a>
-              <h2>
-                {{$zapatillas->marca_zapatilla}} {{$zapatillas->modelo_zapatilla}}
-                <small>{{$zapatillas->color_zapatilla}}</small>
-                  <small>{{$zapatillas->precio_zapatilla}}€ </small>
-
-              </h2>
-            </div>
-            <div class="card-flap flap1">
-              <div class="card-description">
-                Tallas disponibles:
-                <select class="talla_zapatilla" name="talla_zapatilla">
-                  <option value="35">35</option>
-                  <option value="36">36</option>
-                  <option value="37">37</option>
-                  <option value="38">38</option>
-                  <option value="39">39</option>
-                  <option value="40">40</option>
-                  <option value="41">41</option>
-                  <option value="42">42</option>
-                  <option value="43">43</option>
-                  <option value="44">44</option>
-                  <option value="45">45</option>
-                </select>
-              </div>
-              <div class="card-flap flap2">
-                <div class="card-actions">
-                  <a href="#" class="btn">Añadir al carro</a>
-                </div>
-              </div>
+        <div class="card" id="card">
+          <div class="card__image-holder">
+            <img class="card__image" src="https://source.unsplash.com/300x225/?wave" alt="wave"/>
+          </div>
+          <div class="card-title">
+            <a href="#" class="toggle-info btn">
+              <span class="left"></span>
+              <span class="right"></span>
+            </a>
+            <h2>
+              {{$zapatillas->marca_zapatilla}} {{$zapatillas->modelo_zapatilla}}
+              <small>{{$zapatillas->color_zapatilla}}</small>
+              <small>{{$zapatillas->precio_zapatilla}}€ </small>
+            </h2>
+          </div>
+          <div class="card-flap flap1">
+            <div class="card-description">
+              Tallas disponibles:
+              <select class="talla_zapatilla" name="talla_zapatilla">
+                <option value="35">35</option>
+                <option value="36">36</option>
+                <option value="37">37</option>
+                <option value="38">38</option>
+                <option value="39">39</option>
+                <option value="40">40</option>
+                <option value="41">41</option>
+                <option value="42">42</option>
+                <option value="43">43</option>
+                <option value="44">44</option>
+                <option value="45">45</option>
+              </select>
             </div>
           </div>
-        @endforeach
+          <div class="card-flap flap2">
+            <div class="card-actions">
+              <a href="#" class="btn">Añadir al carro</a>
+            </div>
+          </div>
+        </div>
+      @endforeach
+    </div>
+   
+    
       
+      
+    
+    
+     
+        
+        
+      
+      
+        
+         
+        
+      
+    
+  
